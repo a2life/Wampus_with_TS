d8c2165e0dd7f064c45ec638d219ba6187bc2783
adachi <dutch@adachis.info>
2021-06-14 17:50:38 -0400

    refactored random numbers generator

--- src/app.tsx
+++ src/app.tsx
@@ -3,70 +3,55 @@ import {useEffect, useState} from "preact/hooks";
 import {HowToPlay} from "./components/howToPlay_modal";
 
 import {caves} from "./components/Caves";
+import {pickNumbersAtRandomExcept} from "./components/randomNumbers";
+
+enum gamePlay { on, won, lost}
+
 
 const startMessage = () => (<p>Welcome to the game of wumpus!</p>)
-const pickNumberAtRandom = (count: number) => Math.floor(Math.random() * count + 1);
 
-const pickLocationsAtRandomExcept = (exceptions: number[], count: number) => {
-    let candidates: number[] = [];
-    let localExceptions = [...exceptions]
-    for (let x = 0; x < count; x++) {
-        let candidate;
-        do {
-            candidate = pickNumberAtRandom(20);
-            console.log('numbers picked are', candidate)
-        }
-        while (localExceptions.includes(candidate))
-        localExceptions = [...localExceptions, candidate]
-        candidates.push(candidate)
-    }
-
-    return candidates;
-}
 const initialArrowCount = 5;
-const initialBatLocations = pickLocationsAtRandomExcept([], 2)
-console.log('initial bat locations', initialBatLocations)
-const initialPitLocations = pickLocationsAtRandomExcept([...initialBatLocations], 2)
-console.log('initial pit locations', initialPitLocations)
-const initialWumpusLocation = pickLocationsAtRandomExcept([...initialBatLocations, ...initialPitLocations], 1)
-console.log('initialwumpusLoation', initialWumpusLocation)
-const initialPlayerLocation = pickLocationsAtRandomExcept([...initialBatLocations, ...initialPitLocations, ...initialWumpusLocation], 1)[0]
-console.log('initial player location', initialPlayerLocation)
+const initialBatLocations = pickNumbersAtRandomExcept([], 2)
+const initialPitLocations = pickNumbersAtRandomExcept([...initialBatLocations], 2)
+const initialWumpusLocation = pickNumbersAtRandomExcept([...initialBatLocations, ...initialPitLocations], 1)
+const initialPlayerLocation = pickNumbersAtRandomExcept([...initialBatLocations, ...initialPitLocations, ...initialWumpusLocation], 1)[0]
 
-const Warning = (warningMessage: string) => (<div>{warningMessage}</div>)
-const PitWarning = Warning('You feel a cold wind blowing from a nearby cavern.');
-const BatWarning = Warning('You smell something terrible nearby.');
-const WumpusWarning = Warning('You hear a rustling.')
-const FellInPit = Warning('YYYIIIIEEEE . . . FELL IN PIT')
-const BatSnatch = Warning('ZAP--SUPER BAT SNATCH! ELSEWHEREVILLE FOR YOU!');
-const BumpedWumpus = Warning('...OOPS! BUMPED A WUMPUS!')
-const WumpusGotYou = Warning('TSK TSK TSK- WUMPUS GOT YOU!')
+const PitWarning = 'You feel a cold wind blowing from a nearby cavern.';
+const BatWarning = 'You smell something terrible nearby.';
+const WumpusWarning = 'You hear a rustling.'
+const FellInPit = 'YYYIIIIEEEE . . . FELL IN PIT'
+const BatSnatch = 'ZAP--SUPER BAT SNATCH! ELSEWHEREVILLE FOR YOU!';
+const BumpedWumpus = '...OOPS! BUMPED A WUMPUS!'
+const WumpusGotYou = 'TSK TSK TSK- WUMPUS GOT YOU!'
+const AskForNextMove = 'What would you like to do next?'
+const ButtonLabelMoveTo = "Move to"
+const ButtonLabelShoot = "Shoot Arrow"
+const ButtonLabelReset = "Start Over"
 
 const isHazardPresent = (hazardLocation: number[], playerMovingIn: number) => hazardLocation.includes(playerMovingIn)
-const WumpusMoveTo = (playerLocation: number, tunnels: number[]) => {
+const selectNextRoomRandomely = (playerLocation: number, tunnels: number[]) => {
     const moveToOptions = [...tunnels, playerLocation]
     return moveToOptions[Math.floor(Math.random() * moveToOptions.length)]
 }
 
 export function App() {
-    //  const initialBatLocations = pickLocationsAtRandomExcept([],2)
-    const [gamePlaying, setGamePlaying] = useState(true);
+    //  const initialBatLocations = pickNumbersAtRandomExcept([],2)
+    const [gamePlaying, setGamePlaying] = useState(gamePlay.on);
     const [arrowCount, setArrowCount] = useState(initialArrowCount);
     const [batLocations, setBatLocations] = useState([...initialBatLocations])
     const [pitLocations, setPitLocations] = useState([...initialPitLocations])
     const [playerLocation, setPlayerLocation] = useState(initialPlayerLocation)
     const [wumpusLocation, setWumpusLocation] = useState(initialWumpusLocation)
-
     const [Message, setMessage] = useState([startMessage]);
 
     const startOverHandler = () => {
         setMessage([startMessage])
-        setPitLocations(pickLocationsAtRandomExcept([], 2))
-        setBatLocations(pickLocationsAtRandomExcept([], 2))
-        setWumpusLocation(pickLocationsAtRandomExcept([], 1))
-        setPlayerLocation(pickLocationsAtRandomExcept([...pitLocations, ...batLocations, ...wumpusLocation], 1)[0])
+        setPitLocations(pickNumbersAtRandomExcept([], 2))
+        setBatLocations(pickNumbersAtRandomExcept([], 2))
+        setWumpusLocation(pickNumbersAtRandomExcept([], 1))
+        setPlayerLocation(pickNumbersAtRandomExcept([...pitLocations, ...batLocations, ...wumpusLocation], 1)[0])
         setArrowCount(initialArrowCount)
-        setGamePlaying(true)
+        setGamePlaying(gamePlay.on)
 
     }
     const MoveToHandler = (e: Event) => {
@@ -79,17 +64,18 @@ export function App() {
         //irritate wumpus if it is in the room
         // and set a new location for Wumpus.
         if (playerIsMovingTo === wumpusLocation[0]) {
-            setWumpusLocation([WumpusMoveTo(playerIsMovingTo, caves[playerIsMovingTo])])
-            // player is moving to room and tunnels that cave number has
+            setWumpusLocation([selectNextRoomRandomely(playerIsMovingTo, caves[playerIsMovingTo])])
+            // player is moving to room . probability of wumpus moving is 75%. Will stay 25% of the time.
         }
         if (batLocations.includes(playerIsMovingTo)) {
             const playerWasMovingto = playerIsMovingTo;
-            const wasMovingTo = () => <p>Moving to {playerWasMovingto}</p>
-            const batSnatch = () => <p>ZAP--SUPER BAT SNATCH! ELSEWHEREVILLE FOR YOU!'</p>
+            const wasMovingTo = () => <p>Moving to {playerWasMovingto}..</p>
+            const batSnatch = () => <p>{BatSnatch}</p>
             const batLocationsLocal = [...batLocations]
-            batLocationsLocal[batLocations.indexOf(playerWasMovingto)] = pickLocationsAtRandomExcept([...wumpusLocation, ...batLocations], 1)[0]
+            batLocationsLocal[batLocations.indexOf(playerWasMovingto)] =
+                pickNumbersAtRandomExcept([...wumpusLocation, ...batLocations], 1)[0]
             setBatLocations([...batLocationsLocal])
-            playerIsMovingTo = pickLocationsAtRandomExcept([...batLocations, ...wumpusLocation, playerLocation], 1)[0]
+            playerIsMovingTo = pickNumbersAtRandomExcept([...batLocations, ...wumpusLocation, playerLocation], 1)[0]
             setMessage([...Message, wasMovingTo, batSnatch, message])
 
         }
@@ -105,17 +91,20 @@ export function App() {
     useEffect(() => {
         let NewMessage = () => {
             if (isHazardPresent(wumpusLocation, playerLocation)) {
-                setGamePlaying(false)
-                return BumpedWumpus
+                setGamePlaying(gamePlay.lost)
+                return (<div>
+                    <div>{BumpedWumpus}</div>
+                    <div>{WumpusGotYou}</div>
+                </div>)
             } else if (isHazardPresent(pitLocations, playerLocation)) {
-                setGamePlaying(false)
-                return FellInPit
+                setGamePlaying(gamePlay.lost)
+                return (<div>{FellInPit}</div>)
             } else if (isHazardPresent(batLocations, playerLocation)) {
-                //this should not happen. this hazard is taken care in moveHandler. setPlaylocation  creates infinate loop. why?
+                //this should not happen.so if this happens, then we have a bug somehere.
                 setPlayerLocation(0)
-                return BatSnatch
+                return (<div>{BatSnatch}</div>)
             }
-
+            //no hazards, fell through to next code section
 
             return (
                 <div>You are in room <span class="badge bg-success">{playerLocation}</span>, there are tunnels
@@ -124,15 +113,17 @@ export function App() {
                     <span class="badge bg-secondary">{caves[playerLocation][1]}</span> and&nbsp;
                     <span class="badge bg-secondary">{caves[playerLocation][2]}</span>.
                     {batLocations.map((bat) => {
-                        if (caves[playerLocation].includes(bat)) return BatWarning
+                        if (caves[playerLocation].includes(bat)) return (<p>{BatWarning}</p>)
                     })}
                     {pitLocations.map((pit) => {
-                        if (caves[playerLocation].includes(pit)) return PitWarning
+                        if (caves[playerLocation].includes(pit)) return (<p>{PitWarning}</p>)
                     })}
                     {wumpusLocation.map((wumpus) => {
-                        if (caves[playerLocation].includes(wumpus)) return WumpusWarning
+                        //if (caves[playerLocation].includes(wumpus))  return WumpusWarning
+                        if (caves[playerLocation].includes(wumpus)) return (<p>{WumpusWarning}</p>)
+
                     })}
-                    <p> What would you like to do next?</p></div>
+                    <p>{AskForNextMove}</p></div>
 
             )
         }
@@ -148,32 +139,26 @@ export function App() {
     return (
         <>
             <Logo/>
-
-
             <div class="container">
                 <h1 class="display-2">Hunt the Wumpus!</h1>
                 <div class="card  mb-4">
                     <div id="history" class="overflow-auto m-4 h4"
-
-                         placeholder="This is where the history goes"
                          style="height:250px;"
-
-
                     >{Message.map((element, i) => element())}</div>
                 </div>
                 {
-                    gamePlaying && caves[playerLocation].map((tunnel) => {
+                    (gamePlaying === gamePlay.on) && caves[playerLocation].map((tunnel) => {
                         return (
-                            <button class="btn btn-primary " onClick={MoveToHandler}>Move to {tunnel} </button>)
+                            <button class="btn btn-primary "
+                                    onClick={MoveToHandler}>{ButtonLabelMoveTo} {tunnel} </button>)
                     })
                 }
 
-                {gamePlaying &&
-
-                <button class="btn btn-danger ">Shoot Arrow <span class="badge bg-secondary">{arrowCount}</span>
+                {(gamePlaying === gamePlay.on) &&
+                <button class="btn btn-danger ">{ButtonLabelShoot}<span class="badge bg-secondary">{arrowCount}</span>
                 </button>
                 }
-                <button class="btn btn-secondary" onClick={startOverHandler}>Start Over</button>
+                <button class="btn btn-secondary" onClick={startOverHandler}>{ButtonLabelReset}</button>
                 <HowToPlay/>
             </div>
 
---
+++ src/components/randomNumbers.ts
@@ -0,0 +1,18 @@
+const pickNumberAtRandom = (count: number) => Math.floor(Math.random() * count + 1);
+
+export const pickNumbersAtRandomExcept = (exceptions: number[], count: number) => {
+    let candidates: number[] = [];
+    let localExceptions = [...exceptions]
+    for (let x = 0; x < count; x++) {
+        let candidate;
+        do {
+            candidate = pickNumberAtRandom(20);
+            console.log('numbers picked are', candidate)
+        }
+        while (localExceptions.includes(candidate))
+        localExceptions = [...localExceptions, candidate]
+        candidates.push(candidate)
+    }
+
+    return candidates;
+}
